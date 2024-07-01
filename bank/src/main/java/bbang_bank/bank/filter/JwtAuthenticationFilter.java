1e8421d1e58514840b091489e0f504ff0fd53cc1
DDDDKo <to1583@naver.com>
2024-07-01 16:01:23 +0900

    jwt 구현

---
+++ bank/src/main/java/bbang_bank/bank/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,88 @@
+package bbang_bank.bank.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import bbang_bank.bank.entity.UserEntity;
+import bbang_bank.bank.provider.JwtProvider;
+import bbang_bank.bank.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal (HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try{
+
+            String token = parseBearerToken(request);
+            if(token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String userId = jwtProvider.validate(token);
+            if(userId == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            
+            UserEntity userEntity = userRepository.findByUserId(userId);
+            if(userEntity == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, null);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            SecurityContextHolder.setContext(securityContext);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String parseBearerToken(HttpServletRequest request) {
+
+        String authorization = request.getHeader("Authorization");
+
+        boolean hasAuthorization = StringUtils.hasText(authorization);
+        if(!hasAuthorization) return null;
+
+        boolean isBearer = authorization.startsWith("Bearer ");
+        if(!isBearer) return null;
+
+        String token = authorization.substring(7);
+        
+        return token;
+
+    }
+
+}
--- bank/src/main/java/bbang_bank/bank/repository/UserRepository.java
+++ bank/src/main/java/bbang_bank/bank/repository/UserRepository.java
@@ -2,7 +2,10 @@ package bbang_bank.bank.repository;
 
 import org.springframework.stereotype.Repository;
 
+import bbang_bank.bank.entity.UserEntity;
+
 @Repository
 public interface UserRepository {
     
+    UserEntity findByUserId (String userId);
 }
